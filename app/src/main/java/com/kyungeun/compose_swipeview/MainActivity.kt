00c8256d3d98f8df20b2915ecc4693cc4e80c5f6
KyungEun No <74607521+shruddms@users.noreply.github.com>
2022-08-23 23:23:42 +0900

    Feat : Add main button

--- app/src/main/java/com/kyungeun/compose_swipeview/MainActivity.kt
+++ app/src/main/java/com/kyungeun/compose_swipeview/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.kyungeun.compose_swipeview
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kyungeun.compose_swipeview.ui.theme.ComposeSwipeViewTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +30,22 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val context = LocalContext.current
+                    Column(
+                        Modifier
+                            .fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Spacer(modifier = Modifier.padding(20.dp))
+                        Button(
+                            onClick = {
+                                context.startActivity(Intent(context, ExpandActivity::class.java))
+                            }) {
+                            Text(getString(R.string.swipe_expand))
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeSwipeViewTheme {
-        Greeting("Android")
     }
 }
