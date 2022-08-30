4062858b957f52beecd2f898999215d03b1d6dee
KyungEun No <74607521+shruddms@users.noreply.github.com>
2022-08-30 18:04:58 +0900

    Design : Add color

--- app/src/main/java/com/kyungeun/compose_swipeview/ui/theme/Color.kt
+++ app/src/main/java/com/kyungeun/compose_swipeview/ui/theme/Color.kt
@@ -2,7 +2,15 @@ package com.kyungeun.compose_swipeview.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val BlackLight = Color(0xFF161617)
+val BlueOnboarding = Color(0xFF6A91AC)
+
+
+val BackgroundLight = Color(0xFFF2F3F5)
+val BackgroundDark = Color(0xFF0A0A0A)
+
+val TextSecondaryDark = Color(0xFF8B8C8F)
+
+val Gray = Color(0xAEAEAEAE)
