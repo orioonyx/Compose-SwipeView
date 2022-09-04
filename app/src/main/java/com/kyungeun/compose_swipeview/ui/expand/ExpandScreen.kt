7de180fae9d5e4e00894fc918cfb195bfa18e4d5
KyungEun No <74607521+shruddms@users.noreply.github.com>
2022-09-05 03:37:52 +0900

    Feat : Add book dialog

--- app/src/main/java/com/kyungeun/compose_swipeview/ui/expand/ExpandScreen.kt
+++ app/src/main/java/com/kyungeun/compose_swipeview/ui/expand/ExpandScreen.kt
@@ -1,6 +1,7 @@
 package com.kyungeun.compose_swipeview.ui.expand
 
 import android.util.Log
+import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
@@ -17,6 +18,7 @@ import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -27,8 +29,11 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.lerp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
+import com.airbnb.lottie.compose.*
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.pagerTabIndicatorOffset
@@ -44,6 +49,8 @@ import com.skydoves.landscapist.coil.CoilImage
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import java.text.DecimalFormat
+import com.airbnb.lottie.compose.LottieAnimation
+import kotlinx.coroutines.delay
 
 private val InfoContainerMaxHeight = 600.dp
 private val InfoContainerMinHeight = 120.dp //title screen height
@@ -57,8 +64,7 @@ fun ExpandScreen(product: Product, onBackPressed: () -> Unit) {
         val infoMinHeightInPixels = with(LocalDensity.current) { InfoContainerMinHeight.toPx() }
         val dragRange = infoMaxHeightInPixels - infoMinHeightInPixels
         val scope = rememberCoroutineScope()
-        val pagerState = remember { PagerState() }
-        pagerState.maxPage = (product.imageList.size - 1).coerceAtLeast(0)
+        val pagerState = rememberPagerState()
         ConstraintLayout(
             Modifier
                 .fillMaxSize()
@@ -83,9 +89,10 @@ fun ExpandScreen(product: Product, onBackPressed: () -> Unit) {
                 0f,
                 openFraction
             )
-
-            Pager(
-                state = pagerState,
+            HorizontalPager(
+                count = product.imageList.size,
+                contentPadding = PaddingValues(horizontal = 0.dp),
+                state = rememberPagerState(),
                 modifier = Modifier
                     .clickable(
                         enabled = infoSheetState.currentValue == SheetState.Open,
@@ -106,9 +113,9 @@ fun ExpandScreen(product: Product, onBackPressed: () -> Unit) {
                         width = Dimension.fillToConstraints
                         height = Dimension.fillToConstraints
                     }
-            ) {
+            ) { page ->
                 CoilImage(
-                    imageModel = product.imageList[pagerState.currentPage],
+                    imageModel = product.imageList[page],
                     // Crop, Fit, Inside, FillHeight, FillWidth, None
                     contentScale = ContentScale.Crop,
                     // shows a placeholder while loading the image.
@@ -215,8 +222,13 @@ fun InfoContainer(product: Product, modifier: Modifier) {
     ) {
         val rowsModifier = Modifier
             .fillMaxWidth()
-            .padding(start = 24.dp, top = 20.dp, end = 24.dp, bottom = 20.dp)
-        TitleRow(title = product.title, subTitle = product.subTitle, category = product.category, modifier)
+            .padding(start = 24.dp, top = 10.dp, end = 24.dp, bottom = 20.dp)
+        TitleRow(
+            title = product.title,
+            subTitle = product.subTitle,
+            category = product.category,
+            modifier
+        )
         Text(
             text = product.info,
             style = MaterialTheme.typography.body2,
@@ -228,9 +240,17 @@ fun InfoContainer(product: Product, modifier: Modifier) {
         listOf(
             R.string.label_price to price,
         ).forEach {
-            InfoItemRow(modifier = rowsModifier, label = it.first, value = it.second)
+            InfoItemRow(
+                modifier = rowsModifier,
+                label = it.first,
+                value = it.second)
         }
-        BookButton()
+
+        val (isDialog, setDialog) = remember { mutableStateOf(false) }
+        BookAlertDialog(isDialog, onDismissRequest = { setDialog(false)})
+        BookButton(
+            finish = { setDialog(true) }
+        )
     }
 }
 
@@ -239,7 +259,7 @@ fun TitleRow(title: String, subTitle: String, category: String, modifier: Modifi
     Row(modifier = modifier) {
         Column(
             modifier = Modifier
-                .padding(start = 24.dp, bottom = 20.dp)
+                .padding(top = 10.dp, start = 24.dp, bottom = 20.dp)
                 .align(Alignment.CenterVertically)
                 .weight(1f)
         ) {
@@ -257,10 +277,10 @@ fun TitleRow(title: String, subTitle: String, category: String, modifier: Modifi
                 modifier = Modifier
                     .wrapContentWidth()
                     .height(25.dp)
-                    .padding(top = 4.dp),
+                    .padding(top = 5.dp),
                 contentPadding = PaddingValues(0.dp),
                 shape = RoundedCornerShape(5.dp),
-                colors = ButtonDefaults.buttonColors(backgroundColor = Gray),
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.DarkGray),
                 elevation = ButtonDefaults.elevation(defaultElevation = 0.dp)
             ) {
                 Text(
@@ -319,10 +339,11 @@ fun HeartAnimation() {
 }
 
 @Composable
-fun BookButton() {
+fun BookButton(
+    finish: () -> Unit
+) {
     Button(
-        onClick = {
-        },
+        onClick = finish,
         modifier = Modifier
             .padding(start = 24.dp, end = 24.dp, top = 16.dp, bottom = 16.dp)
             .height(50.dp)
@@ -340,17 +361,99 @@ fun BookButton() {
 }
 
 @Composable
+fun CustomAlertDialog(
+    onDismissRequest: () -> Unit,
+    properties: DialogProperties = DialogProperties(),
+    content: @Composable () -> Unit,
+) {
+    Dialog(
+        onDismissRequest = onDismissRequest,
+        properties = properties
+    ) {
+        content()
+    }
+}
+
+@Composable
+private fun BookAlertDialog(
+    visible: Boolean,
+    onDismissRequest: () -> Unit,
+) {
+    if (visible) {
+        CustomAlertDialog(onDismissRequest = { onDismissRequest() }) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .clip(RoundedCornerShape(12.dp))
+                    .background(color = Color.White)
+            ) {
+                Text(
+                    modifier = Modifier
+                        .padding(top = 24.dp)
+                        .padding(horizontal = 24.dp)
+                        .align(Alignment.CenterHorizontally),
+                    text = "Book Completed!",
+                )
+                LottieClickAnim()
+                Text(
+                    modifier = Modifier
+                        .padding(12.dp)
+                        .align(Alignment.End)
+                        .clickable {
+                            onDismissRequest()
+                        }
+                        .padding(12.dp),
+                    text = "Ok",
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun LottieClickAnim() {
+    val composition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(R.raw.check)
+    )
+    val lottieAnimatable = rememberLottieAnimatable()
+
+    LaunchedEffect(composition) {
+        lottieAnimatable.animate(
+            composition = composition,
+            clipSpec = LottieClipSpec.Frame(0, 1200),
+            initialProgress = 0f
+        )
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 18.dp)
+    ) {
+        LottieAnimation(
+            composition = composition,
+            progress = {lottieAnimatable.progress},
+            contentScale = ContentScale.FillHeight,
+            modifier = Modifier
+                .width(72.dp)
+                .height(72.dp)
+                .align(Alignment.Center)
+        )
+    }
+}
+
+@Composable
 fun InfoItemRow(@StringRes label: Int, value: String, modifier: Modifier) {
     Row(modifier) {
         Text(
             text = stringResource(label),
-            style = MaterialTheme.typography.body2,
+            style = MaterialTheme.typography.body2.copy(fontWeight = FontWeight.W500),
             modifier = Modifier.width(120.dp),
             color = MaterialTheme.colors.onPrimary
         )
         Text(
             text = value,
-            style = MaterialTheme.typography.body2.copy(fontWeight = FontWeight.W500)
+            style = MaterialTheme.typography.body2.copy(fontWeight = FontWeight.Bold)
         )
     }
 }
